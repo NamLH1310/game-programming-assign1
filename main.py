a3612e93803be46ec8c8e4b2aebcde549569230f
Lê Hoàng Nam <namlehoang15@gmail.com>
2022-09-25 21:17:39 +0700

    add accuracy score

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ class EntitySystem:
     def __init__(self):
         self.entities = set[Zombie]()
         self.deleted_entities = list[Zombie]()
-        self.Kill = 0
+        self.kill_count = 0
+        self.shot_count = 0
 
     def generate_random_zombie(self):
         if len(self.entities) <= MAX_ZOMBIES and random.randint(0, 1) == 1:
@@ -65,22 +66,45 @@ class EntitySystem:
         self.deleted_entities.clear()
 
     def shot(self, x: int, y: int):
+        self.shot_count += 1
         for z in self.entities:
             if 0 < x - z.x < z.w * z.scale and 0 < y - z.y < z.h * z.scale:
                 screams.play()
                 z.is_dead = True
-                self.Kill += 1
+                self.kill_count += 1
                 break
 
 
 def render_end_screen(entities: EntitySystem):
-    game_over_txt = pygame.font.Font(None, 40).render("GAME OVER", True, pygame.Color('white'))
-    retry_txt = pygame.font.Font(None, 35).render("Press R to Retry", True, pygame.Color('white'))
-    quit_txt = pygame.font.Font(None, 35).render("Press Q to Quit", True, pygame.Color('white'))
-    score_txt = pygame.font.Font(None, 35).render("SCORE: " + str(entities.Kill), True, pygame.Color('White'))
+    game_over_txt = pygame. \
+        font. \
+        Font(None, 40). \
+        render('GAME OVER', True, pygame.Color('brown'))
+    retry_txt = pygame. \
+        font. \
+        Font(None, 35). \
+        render('Press R to Retry', True, pygame.Color('brown'))
+    quit_txt = pygame. \
+        font. \
+        Font(None, 35). \
+        render('Press Q to Quit', True, pygame.Color('brown'))
+    score_txt = pygame. \
+        font. \
+        Font(None, 35). \
+        render(f'SCORE: {entities.kill_count}', True, pygame.Color('brown'))
+    if entities.shot_count == 0:
+        entities.shot_count = 1
+    accuracy_txt = pygame. \
+        font. \
+        Font(None, 35). \
+        render(f'ACCURACY: {round((entities.kill_count / entities.shot_count) * 100, None)}%',
+               True,
+               pygame.Color('brown'))
 
     screen.blit(game_over_txt, ((SCREEN_WIDTH - game_over_txt.get_width()) // 2,
-                                (SCREEN_HEIGHT - game_over_txt.get_height()) // 2 - SCREEN_HEIGHT // 15))
+                                (SCREEN_HEIGHT - game_over_txt.get_height()) // 2 - SCREEN_HEIGHT // 20))
+    screen.blit(accuracy_txt, ((SCREEN_WIDTH - accuracy_txt.get_width()) // 2,
+                               (SCREEN_HEIGHT - accuracy_txt.get_height()) // 2 - SCREEN_HEIGHT // 10))
     screen.blit(score_txt, ((SCREEN_WIDTH - score_txt.get_width()) // 2,
                             (SCREEN_HEIGHT - score_txt.get_height()) // 2 - SCREEN_HEIGHT // 7))
 
